f57c4729ca92a972bc6094d66a3492638622cb92
Salvatore Tosti <saltosti@gmail.com>
2019-06-15 08:03:04 -0700

    Cleans up cruft in site.clj

--- src/sprite_slice/site.clj
+++ src/sprite_slice/site.clj
@@ -8,45 +8,17 @@
             [ring.middleware.params :refer [wrap-params]]
             [selmer.parser :refer [render-file]]))
 
-;; (GET "/archive.zip" []
-;;     (let [archive-file (java.io.File. "c:\\home\\sample\\archive.zip")]
-;;       {:status 200
-;;        :body archive-file
-;;        :headers {"Content-Type" "application/zip"
-;;                  "Content-Length" (str (.length archive-file))
-;;                  "Cache-Control" "no-cache"
-;;                  "Content-Disposition" (str "attachment; filename=" (.getName archive-file))}}
-;;       )
-;;     )
-
 (defroutes site-routes
+  (route/resources "/")
+
   (GET "/" []
        (render-file "public/html/index.html" {}))
 
-  (route/resources "/")
-
   (GET "/slicer/:slug" [slug]
-;;        (response/response (io/file "lisplogo_256.png"))
-;;              (response/content-type "image/png"))
-
        (render-file "public/html/slicer.html" {:image (str slug)}))
-
-
-  ;; test with curl -d '{"key, "yes":"ye"}' -H "Content-Type: application/json" -X POST http://localhost:5000/foo
-
-;;   (POST "/foo" request
-;;         (println
-;;           (-> request
-;;               :body
-;;               (get-in ["keykey2"])))
-;; ;;         {"zed","Uploaded user."}
-
-;;         )
 
   (route/not-found "Not Found"))
 
 (def site
   (-> site-routes
       wrap-json-body))
-;;       wrap-json-response
-;;       (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))))
